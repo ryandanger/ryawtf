759a3b97bb9d0cf6ec32ca3d61fffb94a2f39abb
ryandanger <ryan@thedanger.space>
2022-02-24 12:11:11 +0800

    remove site folder

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "hiya",
-        "excerpt":"hiiii uhhhhh ye i guess i don’t really know why i made this blog, or what i’m going to do with it. but hey, it’s here now and i’m typing to it and looking to just fill this page with nonsense. who knows what this shit might bring. i don’t...","categories": ["blog","quick"],
-        "tags": [],
-        "url": "/blog/blog/quick/hiya/",
-        "teaser": null
-      }]
